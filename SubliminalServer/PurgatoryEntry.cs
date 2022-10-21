7f715b257b15b5db4abbc80ff832a2e3cea4cf86
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2022-10-21 10:59:33 +0100

    Replace centere wide with poem centre wide

--- SubliminalServer/PurgatoryEntry.cs
+++ SubliminalServer/PurgatoryEntry.cs
@@ -43,7 +43,7 @@ public record PurgatoryEntry
     "poemName": "Test",
     "poemAuthor": "Zekiah",
     "poemContent": "Example",
-    "pageStyle": "centre-wide",
+    "pageStyle": "poem-centre-wide",
     "pageBackground": ""
 }
  */
