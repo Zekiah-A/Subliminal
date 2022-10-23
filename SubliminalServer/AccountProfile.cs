c0fa37952537521b0aedab9d18562c79cb3c1f92
Zekiah-A <zekiahamoako@outlook.com>
2022-10-23 19:11:34 +0100

    Add profile avatar URL

--- SubliminalServer/AccountProfile.cs
+++ SubliminalServer/AccountProfile.cs
@@ -11,8 +11,9 @@ public record AccountProfile()
     [JsonInclude] public string Location { get; set; }
     [JsonInclude] public string[] PinnedPoems { get; set; }
     [JsonInclude] public string Role { get; set; }
+    [JsonInclude] public string AvatarUrl { get; set; }
     
-    //Immutable, does not have to be serialisable
+    //Immutable, you can't change these, does not have to be serialisable
      public string JoinDate { get; set; }
      public AccountBadge[] Badges { get; set; }
      public List<string>? PoemGuids { get; set; }
