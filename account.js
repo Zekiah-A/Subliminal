7007361bfeae35889f4c7860f530aa5398b27864
Zekiah-A <zekiahamoako@outlook.com>
2022-11-10 23:54:39 +0000

    [WIP]: Move profile page to execute page action

---
+++ account.js
@@ -0,0 +1,22 @@
+const actionType = {
+    // General account actions
+    BlockUser: 0,
+    UnblockUser: 1,
+    FollowUser: 2,
+    UnfollowUser: 3,
+    LikePoem: 4,
+    UnlikePoem: 5,
+    
+    // Location - Account data
+    UpdateEmail: 6,
+    UpdateNumber: 7,
+    
+    //Location - Account profile
+    UpdatePenName: 8,
+    UpdateBiography: 9,
+    UpdateLocation: 10,
+    UpdateRole: 11,
+    UpdateAvatar: 12,
+    PinPoem: 13,
+    UnpinPoem: 14
+}
