d52faf46707b695980d19ed47a683a85ccde6b85
Zekiah-A <zekiahamoako@outlook.com>
2023-03-11 16:00:02 +0000

    [Massive] start moving poem editor to canvas document

--- editor-document.js
+++ editor-document.js
@@ -12,10 +12,18 @@ const styleCodes = {
     subscript: '\x08',
 }
 
+const positionMovements = {
+    up: 0,
+    down: 1,
+    left: 2,
+    right: 3,
+    none: 4
+}
+
 class EditorDocument {
     constructor(data) {
         this.data = data || ""
-        this.position = 0 // Raw position
+        this.position = this.data.length // Raw position
         this.selection = { position: 0, end: 0 } // Raw position
         this.workingStyles = []
         this.encoder = new TextEncoder()
@@ -78,6 +86,98 @@ class EditorDocument {
         return html.join("")
     }
 
+    renderCanvasData(canvas) {
+        const context = canvas.getContext("2d")
+        const encoded = this.encoder.encode(this.data)
+        const buffer = new ArrayBuffer(encoded.length)
+        for (let i = 0; i < encoded.length; i++) {
+            buffer[i] = encoded[i]
+        }
+        const view = new DataView(buffer)
+
+        const fontSize = 18
+
+        let inStyle = false
+        let stylesStack = [] // Every style on this stack gets applied
+        let colourStack = [] // Only the top style on this stack should be applied
+        let lines = []
+        let currentLine = ""
+
+        // We precalculate how many lines the canvas will be 
+        let preCalcHeight = Math.max(360, this.getLines(this.data).length * fontSize)
+        canvas.style.height = preCalcHeight + "px"
+        canvas.height = preCalcHeight
+        context.clearRect(0, 0, canvas.width, canvas.height)
+
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i] == '\x00') {
+                inStyle = !inStyle
+                continue
+            }
+            else if (this.data[i] == '\x01') {
+                lines.push(currentLine)
+                currentLine = ""
+                continue
+            }
+            else if (this.data[i] == '\x02') {
+                if (stylesStack.pop() == styleCodes.colour) {
+                    colourStack.pop()
+                }
+                continue
+            }
+            
+            if (inStyle) {
+                stylesStack.push(data[i])
+
+                if (data[i] == styleCodes.colour) {
+                    colourStack.push(view.getUint32(data[i + 1]))
+                    i += 4
+                }
+            }
+            else {
+                // We draw characters one by one, instead of line by line so that we can make
+                // sure to apply the correct style onto each.
+                
+                if (colourStack.length > 0) {
+                    context.fillStyle = "#" + colourStack[colourStack.length - 1]
+                }
+
+                context.font = fontSize + "px Arial, Helvetica, sans-serif"
+                let measure = context.measureText(currentLine)
+
+                // +fontSize is slightly incorrect, as it is not exactly the height of this line, but will work well enough v
+                context.fillText(this.data[i], measure.width, (lines.length + 1) * fontSize)
+                currentLine += this.data[i]
+            }
+        }
+        // Final line won't be pushed, so we do it now
+        lines.push(currentLine)
+        currentLine = ""
+
+        // Calculate line count up to position.
+        let positionLineIndex = 0
+        for (let i = 0; i < this.position; i++) {
+            if (this.data[i] == '\x01') {
+                positionLineIndex++
+            }
+        }
+
+        // We also count up all chars in the lines before position,
+        // then negate from position to get position on this (position's) line.
+        let beforePositionLine = 0
+        for (let before = 0; before < positionLineIndex; before++) {
+            beforePositionLine += lines[before].length
+        }
+
+        let positionLine = lines[positionLineIndex]
+        // TODO: We are measuring text here as if the cursor position line is made up of all default chars, we need to
+        // instead loop over each, checking against the styles to ensure we are actually getting it correctly
+        context.font = fontSize + "px Arial, Helvetica, sans-serif"
+        let positionMeasure = context.measureText(positionLine.slice(0, this.position - beforePositionLine))
+
+        context.fillRect(positionMeasure.width, positionLineIndex * fontSize + 2, 2, fontSize)
+    }
+
     optimiseData(data) {
         let i = 0
         while (i < data.length - 1) {
@@ -136,6 +236,66 @@ class EditorDocument {
         }
 
         return rawI
+    }
+
+    movePosition(movement) {
+        switch (movement) {
+            case positionMovements.left:
+                this.position = Math.max(0, this.position - 1)
+                break
+            case positionMovements.right:
+                // console.log(this.positionInLine(this.position, this.getLines(this.data)))
+                break
+        }
+    }
+
+    // Static
+    positionInLine(globalIndex, lines) {
+        // Count all lines before position
+        let linesCharLength = 0
+        for (let i = 0; i < lines.length - 1; i++) {
+            linesCharLength += lines[i].length
+        }
+
+        return globalIndex - linesCharLength
+    }
+
+    // Static 
+    getLineHeights(globalIndex, lines, spacing = 0) {
+        let lineHeights = 0
+        for (let line of lines.slice(0, globalIndex)) {
+            let heightMeasure = context.measureText(line)
+            lineHeights += heightMeasure.actualBoundingBoxAscent + heightMeasure.actualBoundingBoxDescent
+            lineHeights += spacing
+        }
+        return lineHeights    
+    }
+
+    // Static
+    getLines(data) {
+        let lines = []
+        let current = ""
+        let inStyle = false
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] == '\x00') {
+                inStyle = !inStyle
+                continue
+            }
+            if (data[i] == '\x01') {
+                lines.push(current)
+                current = ""
+                continue
+            }
+
+            if (!inStyle) {
+                current += data[i]
+            }
+        }
+        lines.push(current)
+        current = ""
+
+        return lines
     }
 
     addStyle(code, value = null) {
@@ -242,6 +402,7 @@ class EditorDocument {
                 && this.selection.end - this.selection.position != 0)
     }
 
+    // Static
     inStyle(position, data) {
         let count = 0
         for (let i = 0; i < data.slice(0, position); i++) {
