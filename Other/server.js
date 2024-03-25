d0438ab24e8364ed731665001275c01f747cbc41
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2024-03-25 10:41:45 +0000

    Make classics sections more accessible + keyboard navigation fixes

--- Other/server.js
+++ Other/server.js
@@ -1,2 +1,5 @@
-const serverBaseAddress = "http://localhost:1234"//"https://server.poemanthology.org/subliminal"
-const editServerAddress = "http://localhost:1235"//"https://server.poemanthology.org/subliminaledit"
+const serverBaseAddress =
+    localStorage.server || "https://server.poemanthology.org/subliminal"
+const editServerAddress =
+    localStorage.editServer ||
+    "https://server.poemanthology.org/subliminaledit"
